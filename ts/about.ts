82da2065bd8d7e391e22e16ef6a59c9e3e5c0edf
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2020-01-24 13:38:43 -0300

    Code cleanup

--- ts/about.ts
+++ ts/about.ts
@@ -16,9 +16,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 *****************************************************************************/
-var _b = require('electron');
 
 function licensesClicked() {
+    var _b = require('electron');
     _b.ipcRenderer.send('licenses-clicked');
-   console.log('clicked');
 }
--- ts/licenses.ts
+++ ts/licenses.ts
@@ -16,8 +16,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 *****************************************************************************/
-var _c = require('electron');
 
 function openLicense(type: string) {
+    var _c = require('electron');
     _c.ipcRenderer.send('open-license', {type: type});
 }
