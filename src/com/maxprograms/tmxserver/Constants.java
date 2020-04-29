87425e7bec8ed799626420662cd86e9c303bc57d
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2020-04-29 09:48:25 -0300

    Updated OpenXLIFF and build date

--- src/com/maxprograms/tmxserver/Constants.java
+++ src/com/maxprograms/tmxserver/Constants.java
@@ -25,8 +25,8 @@ public class Constants {
 	}
 
 	public static final String APPNAME = "TMXEditor";
-	public static final String VERSION = "2.0.0";
-	public static final String BUILD = "20200221_2104";
+	public static final String VERSION = "2.0.2";
+	public static final String BUILD = "20200429_0946";
 
 	public static final String REASON = "reason";
 	public static final String STATUS = "status";
