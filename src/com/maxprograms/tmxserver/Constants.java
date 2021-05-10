38711412f2a350680b5b47481f1c0d60eaca7252
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-05-10 08:23:42 -0300

    Updated java build number

--- src/com/maxprograms/tmxserver/Constants.java
+++ src/com/maxprograms/tmxserver/Constants.java
@@ -25,8 +25,8 @@ public class Constants {
 	}
 
 	public static final String APPNAME = "TMXEditor";
-	public static final String VERSION = "2.4.2";
-	public static final String BUILD = "20210412_1458";
+	public static final String VERSION = "2.4.3";
+	public static final String BUILD = "20210506_1835";
 
 	public static final String REASON = "reason";
 	public static final String STATUS = "status";
